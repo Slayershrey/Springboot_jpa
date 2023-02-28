3036c4b6eed8747ad1144667b80aefe5f9dac80f
Slayershrey <79738114+Slayershrey@users.noreply.github.com>
2023-03-01 00:18:31 +0530

    Add files via upload

---
+++ spring-boot-prac/spring-boot-prac/src/main/java/com/pracSpringboot/springbootprac/SpringBootPracApplication.java
@@ -0,0 +1,15 @@
+package com.pracSpringboot.springbootprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPracApplication.class, args);
+	}
+
+}
